7485c5c5509e61b5ddb671dc55fec40b05347c6f
WolfBolin <wolfbolin@foxmail.com>
2020-10-02 10:37:27 +0800

    Add logger and Echo

--- service/service.py
+++ service/service.py
@@ -40,6 +40,7 @@ app.mysql_pool = PooledDB(creator=pymysql, **mysql_config, **pool_config)
 
 @app.route('/')
 def hello_world():
+    app.logger.info('Trigger "Hello,world!"')
     return "Hello, world!"
 
 
@@ -57,6 +58,7 @@ def user_login():
         user_info["time"] = Util.unix2timestamp(unix_time, pattern="%H:%M")
     else:
         return abort(400)
+    app.logger.info("User try to login: {}".format(user_info["username"]))
 
     # 查询并写入数据
     conn = app.mysql_pool.connection()
@@ -98,6 +100,7 @@ def get_user_list():
         result.append(item)
     return {
         "status": "success",
+        "message": "列表读取成功",
         "data": result
     }
 
@@ -113,6 +116,7 @@ def user_logout():
         pass
     else:
         return abort(400)
+    app.logger.info("User try to  logout: {}".format(user_info["username"]))
 
     # 检查并删除任务
     conn = app.mysql_pool.connection()
@@ -141,6 +145,7 @@ def check_list(check_time=None):
     else:
         unix_time = Util.timestamp2unix("2020-01-01 " + check_time + ":00")
         time_now = Util.unix2timestamp(unix_time, pattern="%H:%M")
+    app.logger.info("Check time point at {}".format(time_now))
     conn = app.mysql_pool.connection()
     # 查询当前时间需要打开的用户
     cursor = conn.cursor(pymysql.cursors.DictCursor)
@@ -153,7 +158,30 @@ def check_list(check_time=None):
         if user_info["sms"] == "YES":
             Util.send_sms_message(user_info["nickname"], user_info["phone"], str(data))
         Util.write_log(conn, user_info["username"], status, data, run_err)
-    return "Check time: %s" % time_now
+    app.logger.info("Check point {}, Done".format(time_now))
+
+    return jsonify({
+        "status": "success",
+        "message": "Check time: {}".format(time_now)
+    })
+
+
+@app.errorhandler(400)
+def http_forbidden(msg):
+    app.logger.warning("{}: <HTTP 400> {}".format(request.url, msg))
+    return jsonify({
+        "status": "error",
+        "message": "请求数据异常，请检查",
+    })
+
+
+@app.errorhandler(500)
+def http_forbidden(msg):
+    app.logger.warning("{}: <HTTP 400> {}".format(request.url, msg))
+    return jsonify({
+        "status": "error",
+        "message": "服务器状态异常，请联系开发者",
+    })
 
 
 if __name__ == '__main__':
