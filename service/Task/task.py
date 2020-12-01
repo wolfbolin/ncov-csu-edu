7c0f93ac3f678bfc7a86d3facea1ecddf7a285bf
WolfBolin <wolfbolin@foxmail.com>
2020-12-02 00:17:14 +0800

    Fix sign_time BUG

--- service/Task/task.py
+++ service/Task/task.py
@@ -137,7 +137,7 @@ def user_sign_in(config, user_info, sms_token):
         # 任务完成
         Kit.write_log(conn, 'user_check', user_info["username"], status, data, run_err)
         sql = "UPDATE `task` SET `status`=%s, `sign_time`=%s WHERE `username`=%s AND `date` = CURDATE()"
-        cursor.execute(sql, args=["success", "00:01:02", user_info["username"]])
+        cursor.execute(sql, args=["success", Kit.str_time("%H:%M:%S"), user_info["username"]])
         conn.commit()
     except BaseException as e:
         Kit.print_red(e)
